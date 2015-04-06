c2c446488857e20c115c23135effc1657c618761
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2015-04-07 03:01:22 +1000

    Basic set up

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,13 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'IBScrollViews'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
-
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
-
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = navigationController
+    storyboard = UIStoryboard.storyboardWithName("Storyboard", bundle: nil)
+    @window.rootViewController = storyboard.instantiateInitialViewController
     @window.makeKeyAndVisible
 
     true
---
+++ app/main_view_controller.rb
@@ -0,0 +1,7 @@
+class MainViewController < UIViewController
+  include IB
+
+  outlet :red_view, UIView
+  outlet :blue_view, UIView
+  outlet :green_view, UIView
+end
